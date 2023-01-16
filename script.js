555599c110b9583e2a81cc98bce47b48e72c0345
djuric24 <djuric24a@gmail.com>
2023-01-16 22:59:17 +0100

    Cookies

---
+++ script.js
@@ -0,0 +1,22 @@
+document.querySelector('#setCookie').addEventListener('click', e => {
+    e.preventDefault();
+
+    document.cookie = "name=Aleksandar";
+    document.cookie = "test=test1";
+
+    const date = new Date();
+
+    let today = date.getTime();
+    let expires = 2 * 24 * 60 * 60 * 1000;
+
+    date.setTime(today + expires);
+
+    let new_date = date.toUTCString();
+
+
+    document.cookie = `login=1; expires=${new_date}`;
+
+    console.log(document.cookie);
+});
+
+
